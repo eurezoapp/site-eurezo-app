d29a78a732adc96be005058a61963f912068540f
eurezoapp <guthierres@icloud.com>
2025-01-25 17:47:22 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -31,7 +31,7 @@ import { ConsagracaoAparecida } from './components/ConsagracaoAparecida';
 import { AntifonasNossaSenhora } from './components/AntifonasNossaSenhora';
 import { UpdateNotification } from './components/UpdateNotification';
 
-const APP_VERSION = '1.0.3';
+const APP_VERSION = '1.0.4';
 
 const App: React.FC = () => {
   const [fontSize, setFontSize] = useState(16);
