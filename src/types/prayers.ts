78923ef6f907e81597ca8ae49c6fe8db92290348
eurezoapp <guthierres@icloud.com>
2025-01-26 21:30:19 -0300

    Update prayers.ts

--- src/types/prayers.ts
+++ src/types/prayers.ts
@@ -174,3 +174,110 @@ divina virtute, in infernum detrude.
 Amen.`
   }
 };
+
+export const atoContricao = {
+  title: "ATO DE CONTRIÇÃO",
+  subtitle: "(cf. Ritual da Penitência, Rito Romano)",
+  prayers: [
+    {
+      prayer: `Senhor, eu me arrependo sinceramente
+de todo o mal que pratiquei
+e do bem que deixei de fazer.
+Pecando, eu vos ofendi, meu Deus, sumo bem,
+digno de ser amado sobre todas as coisas.
+Proponho firmemente, ajudado por vossa graça,
+fazer penitência, não mais pecar
+e fugir às ocasiões do pecado.
+Senhor, tende piedade de mim,
+pelos méritos da paixão
+de nosso Salvador, Jesus Cristo.
+Amém.`
+    },
+    {
+      rubric: "(Sl 24(25), 6-7)",
+      prayer: `Recordai, Senhor meu Deus, vossa ternura
+e a vossa compaixão que são eternas!
+Não recordeis os meus pecados quando jovem,
+nem vos lembreis de minhas faltas e delitos!
+De mim lembrai-vos porque sois misericórdia
+e sois bondade sem limites, ó Senhor! 
+Amém.`
+    },
+    {
+      rubric: "(Sl 50 (51), 4-5)",
+      prayer: `Lavai-me todo inteiro do pecado,
+e apagai completamente a minha culpa!
+Eu reconheço toda a minha iniquidade,
+o meu pecado está sempre à minha frente.
+Amém.`
+    },
+    {
+      rubric: "(cf. Lc 15,18; 18,13)",
+      prayer: `Pai, pequei contra ti;
+Já não mereço ser chamado teu filho.
+Tende piedade de mim, que sou pecador!
+Amém.`
+    },
+    {
+      prayer: `Deus, Pai de misericórdia,
+como filho(a) arrependido(a) regresso para dizer-vos:
+"Pequei contra vós;
+Já não sou digno de ser chamado voss(o) filh(o)".
+Cristo Jesus, Salvador do mundo,
+eu vos peço como o ladrão arrependido,
+"Lembrai-vos de mim, Senhor,
+quando estiverdes no vosso Reino".
+Espírito Santo, fonte de amor,
+cheio de confiança vos suplico:
+Purificai o meu coração
+e concedei-me viver como filho da luz".
+Amém.`
+    },
+    {
+      prayer: `Senhor Jesus,
+que abristes os olhos dos cegos e curastes os enfermos,
+que perdoastes a pecadora,
+e confirmastes Pedro em vosso amor, após o seu pecado,
+atendei à minha súplica:
+perdoai todos os meus pecados,
+renovai em mim o vosso amor,
+e concedei-me viver em caridade fraterna
+para que possa anunciar a todos a vossa salvação.
+Amém.`
+    },
+    {
+      prayer: `Senhor Jesus,
+que quisestes ser chamado o amigo dos pecadores,
+livrai-me dos meus pecados
+pelo mistério de vossa morte e ressurreição.
+Que a vossa paz permaneça em mim,
+para que eu possa produzir frutos
+de caridade, justiça e verdade.
+Amém.`
+    },
+    {
+      prayer: `Senhor Jesus Cristo,
+Cordeiro de Deus que tirais o pecado do mundo,
+dignai-vos reconciliar-nos com vosso Pai,
+pela graça do Espírito Santo;
+purificai-me em, vosso sangue, de todo o pecado,
+e fazei-me renascer para uma vida nova,
+a fim de proclamar a vossa glória.
+Amém.`
+    },
+    {
+      prayer: `Meu Deus, tende misericórdia de mim
+na vossa bondade;
+desviai a vossa face dos meus pecados;
+apagai a minha iniquidade,
+criai em mim um coração que seja puro,
+e dai-me um espírito reto.
+Amém.`
+    },
+    {
+      prayer: `Senhor Jesus, filho de Deus,
+tende piedade de mim, que sou um(a) pecador(a).
+Amém.`
+    }
+  ]
+};
